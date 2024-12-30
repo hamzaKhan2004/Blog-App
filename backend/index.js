37fa1db9e6e38869675c499f04fd5ff1ca56d508
hamzaKhan2004 <khanhamzatz@gmail.com>
2024-12-30 10:57:40 +0530

    Signup Api Created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Mongodb Is Connected"))
   .catch((error) => console.log("Mongodb Error : ", error.message));
+
 const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () =>
   console.log("Server Is Running On Port http://localhost:3000")

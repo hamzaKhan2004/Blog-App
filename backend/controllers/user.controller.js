dac7a5fa07845468366e628387c5da2b5be2c503
hamzaKhan2004 <khanhamzatz@gmail.com>
2025-01-13 22:14:57 +0530

    show the comments of the post

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -58,7 +58,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if ( !req.user.isAdmin && req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
@@ -118,4 +118,15 @@ export const getUsers = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {}
 };

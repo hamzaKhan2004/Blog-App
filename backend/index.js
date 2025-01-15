d0a5a6abd4ebe6f1b36bff79799c893c95560b31
hamzaKhan2004 <khanhamzatz@gmail.com>
2025-01-16 00:35:58 +0530

    pre installation

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,15 @@ import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import path from "path";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Mongodb Is Connected"))
   .catch((error) => console.log("Mongodb Error : ", error.message));
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,6 +30,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 //comment routes
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
